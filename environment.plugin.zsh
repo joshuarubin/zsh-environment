b6d4bd270dc89696f5d9bc67bdb96536d8bc4727
Joshua Rubin <jrubin@zvelo.com>
2016-05-23 23:47:24 -0600

    check for running jobs before exiting zsh

--- environment.plugin.zsh
+++ environment.plugin.zsh
@@ -31,7 +31,7 @@ setopt AUTO_RESUME        # Attempt to resume existing job before creating a new
 setopt NOTIFY             # Report status of background jobs immediately.
 unsetopt BG_NICE          # Don't run all background jobs at a lower priority.
 unsetopt HUP              # Don't kill jobs on shell exit.
-unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
+setopt CHECK_JOBS         # Don't report on jobs when shell exit.
 
 #
 # Termcap
